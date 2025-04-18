18817d21e08adfcfd7d8f613276c1b513c330f2b
Haitam Chouiekh <chouiekh@uwm.edu>
2025-04-18 13:51:03 -0500

    Added JavaFx

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX!");
+        Scene scene = new Scene(label, 400, 300);
+
+        primaryStage.setTitle("Stock Visualizer");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
+        launch(args);
         System.out.println("Hello world!");
     }
 }
